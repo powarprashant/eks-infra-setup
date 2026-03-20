744d4d2080b1ced056ec371b8ad8f1cd0efd68d6
powarprashant <prashantkpowar24@gmail.com>
2026-03-20 18:01:08 +0530

    vpc

--- modules/vpc/vpc.tf
+++ modules/vpc/vpc.tf
@@ -23,11 +23,3 @@ resource "aws_subnet" "private_subnet" {
 output "vpc_id" {
   value = aws_vpc.cloudcart_vpc.id
 }
-
-output "public_subnet" {
-  value = aws_subnet.public_subnet.id
-}
-
-output "private_subnet" {
-  value = aws_subnet.private_subnet.id
-}
