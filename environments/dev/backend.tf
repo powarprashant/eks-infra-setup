a63a80aa258c112def1ef0f5a69ddcc8ecf7369f
powarprashant <prashantkpowar24@gmail.com>
2026-03-31 11:39:00 +0530

    updated backend.tf

--- environments/dev/backend.tf
+++ environments/dev/backend.tf
@@ -3,14 +3,13 @@ terraform {
 required_version = ">= 1.5"
 
 backend "s3" {
-bucket = "cloudcart-terraform-state"
-key    = "dev/terraform.tfstate"
-region = "ap-south-1"
+bucket         = "cloudcart-terraform-state"
+key            = "dev/terraform.tfstate"
+region         = "ap-south-1"
 
-```
-# ✅ New locking mechanism
-use_lockfile = true
-```
+
+# ✅ Correct locking mechanism
+dynamodb_table = "terraform-lock"
 
 }
 }
